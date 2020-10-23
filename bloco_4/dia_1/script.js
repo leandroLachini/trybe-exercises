c1f9eae98d2293166bad66a719912cc1c4d20e8f
Leandro Lachini <leandrolachini@gmail.com>
2020-10-23 19:41:53 -0300

    commit final do dia

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,66 @@
+// Variaveis
+
+let a = 5;
+let b = 10;
+let c = 25;
+let d = 15;
+
+// Exercicio 1
+
+console.log(a+b); // adicao
+console.log(a-b); // subtracao
+console.log(a*b); // mutiplicacao
+console.log(a/b); // divisao
+console.log(a%b); // modulo
+
+// Exercicio 2
+
+if (a > b) {
+    console.log("a é maior que b");
+  } else {
+    console.log("b é maior que a");
+  };
+
+// Exercicio 3
+
+if (a > b && a > c) {
+    console.log("O maior número é (a):" & a);
+  } else if (b > a && b > c) {
+    console.log("O maior número é (b):" & b);
+  } else {
+    console.log("O maior número é (c):" & c);
+  };
+
+
+// Exercicio 4
+
+if (d > 0) {
+    console.log("positive");
+  } else if (d < 0) {
+    console.log("negative");
+  } else {
+    console.log("zero");
+  };
+
+// Exercicio 5
+
+let angT1 = 60; // 1 angulo triangulo
+let angT2 = 60; // 2 angulo triangulo
+let angT3 = 50; // 3 angulo triangulo
+
+
+let somaAngulos = angT1 + angT2 + angT3;
+
+if (somaAngulos === 180) {
+  console.log("Angulos validos!");
+} else {
+  console.log("Angulos Invalidos!");
+};
+
+
+
+// Exercicio 6
+
+
+
+
