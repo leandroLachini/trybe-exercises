f4ebb08e53ddb1083055d4187aca7c100d7e9c8c
Leandro Lachini <leandrolachini@gmail.com>
2020-10-28 19:28:01 -0300

    Exercicio bloco_4 dia_4

---
+++ bloco_4/dia_4/exe4.js
@@ -0,0 +1,15 @@
+// Exercicio 1
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente= 'Sim';
+homem.recorrente= 'Sim';
+// console.log(info)
+
+for( let key in info){
+  console.log(info[key]);
+}
