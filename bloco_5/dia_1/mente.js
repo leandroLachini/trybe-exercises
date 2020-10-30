1db841e6924c25c186f314e8806b6f027d7185d9
Leandro Lachini <leandrolachini@gmail.com>
2020-10-30 19:34:58 -0300

    exercicio aula

---
+++ bloco_5/dia_1/mente.js
@@ -0,0 +1,3 @@
+document.getElementById('page-title').innerText = 'The pursuit of hapiness';
+document.getElementById('second-paragraph').innerText = 'HAPPY';
+document.getElementById('subtitle').innerText = 'BE';
