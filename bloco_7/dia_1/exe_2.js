882bbd762dee1416e137fc1d12080ad24878b31c
Leandro Lachini <leandrolachini@gmail.com>
2020-11-24 14:55:00 -0300

    exe2 e part_2_exe3 finalizados

--- bloco_7/dia_1/exe_2.js
+++ bloco_7/dia_1/exe_2.js
@@ -1,9 +1,17 @@
-const oddsAndEvens = ([13, 3, 4, 10, 7, 2]) => {
-  const ascendingNum = oddsAndEvens.sort();
-  return ascendingNum;
-};
+// const oddsAndEvens = (array) => {
+//   const ascendingNum = array.sort();
+//   return ascendingNum;
+// };
 
 // Seu código aqui.
 
 // console.log(ascendingNum);
 
+// const x = oddsAndEvens([13, 3, 4, 10, 7, 2]);
+// console.log(x);
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort((a, b) => a - b); 
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente !`);
+  
--- bloco_7/dia_1/part_2_exe3.js
+++ bloco_7/dia_1/part_2_exe3.js
@@ -1,12 +1,12 @@
 const button = document.getElementById('btn');
 const divCount = document.getElementById('count');
-console.log(button);
+// console.log(button);
+let clickCount = 0;
 button.addEventListener('click', (event) => {
   if (event.target){
-    let clickCount = 0;
     clickCount += 1;
     divCount.innerHTML = clickCount;
   }
-  console.log(clickCount);
+  // console.log(clickCount);
   return clickCount;
  });
