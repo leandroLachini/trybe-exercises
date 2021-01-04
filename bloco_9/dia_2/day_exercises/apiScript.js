451b8138f2cc6e7dec4ab9c96bd5f8f9e2897673
Leandro Lachini <leandrolachini@gmail.com>
2021-01-04 14:53:15 -0300

    aula

---
+++ bloco_9/dia_2/day_exercises/apiScript.js
@@ -0,0 +1,12 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  fetch(API_URL, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      document.getElementById('jokeContainer').innerText = data.joke
+    );
+  // Adicionar lógica aqui!
+};
+
+window.onload = () => fetchJoke();
